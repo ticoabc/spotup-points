bab622e15f94f5b0a7243791906c4cc752841dd0
Tiago de Freitas <tiagoabc@gmail.com>
2023-09-11 19:45:42 -0300

    Created projct

---
+++ src/test/java/com/spotup/points/PointsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spotup.points;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PointsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
